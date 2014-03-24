f79843f97546226a14f487e8b36ca4dc135533cd
nika-nalle <nikaandreenkova@yandex.ru>
2014-03-24 22:51:59 +0400

    Upload photos

--- form.php
+++ form.php
@@ -4,3 +4,22 @@
 <p><input type="text" name="title" value="title"></p>
 <input type="submit" name="submit" value="Submit">
 </form>
+<?php
+$uploads_dir = __DIR__ . '/data/' . $_FILES['photo']['name'];
+move_uploaded_file ($_FILES['photo']['tmp_name'], $uploads_dir);
+$conn = new PDO (
+'mysql:host=localhost;dbname=epic_gallery;charset=utf8','root', '');
+$conn->setAttribute(
+PDO::ATTR_ERRMODE,
+PDO::ERRMODE_EXCEPTION
+);
+if (!empty($_POST)) {
+$stmt = $conn->prepare('INSERT INTO photos(name, alt, title) VALUES (:name, :alt, :title)');
+$stmt->execute(array(
+':alt' => $_POST['alt'],
+':title' => $_POST['title'],
+':name' => $_FILES['photo']['name'],
+));
+}
+
+?>
