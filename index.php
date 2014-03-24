60c94d4454d5d11bd009bf9ad94d0d6d01f773b5
mr-sander <murashkosash@gmail.com>
2014-03-24 22:02:18 +0400

    index file

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+header('Content-type: text/html; charset=utf-8');
+ ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	$action = isset($_GET['action']) ? htmlentities($_GET['action']) : 0;
+	switch($action){
+		case 'list.php':
+
+			break;
+		case 'add.php':
+
+			break;
+		case 'edit.php':
+
+			break;
+		case 'delete.php':
+		
+			break;
+		default:
+			echo "Error 404";
+			break;
+	}
+	 ?>
+</body>
+</html>
